7dd7dc175975361e83e9f2c91c2a8c350759a283
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-12-10 16:58:41 +0300

    lab3_py_realisation done

---
+++ lab3/lab3_py.py
@@ -0,0 +1,32 @@
+def get_numbers():
+    max_size = 10
+    nums_arr = []
+    while len(nums_arr) < max_size:
+        print(f'Enter next number ({max_size - len(nums_arr)} remains):')
+        num = input()
+        if is_correct(num):
+            nums_arr.append(int(num))
+        else:
+            print('Incorrect input. Try again.\n')
+    return nums_arr
+
+
+def is_correct(num: str):
+    if num.isdigit() or (num[0] == '-' and num[1:].isdigit()):
+        return True
+    return False
+
+
+def process_array(nums_arr: list):
+    sum = 0
+    pos_lst = [num for num in nums_arr if num >= 0]
+    neg_lst = [num for num in nums_arr if num < 0]
+    for pos, neg in zip(pos_lst, neg_lst):
+        sum += pos * neg
+    return sum
+
+
+if __name__ == '__main__':
+    numbers = get_numbers()
+    print(numbers)
+    print(process_array(numbers))

e16799786c116c0501b6a781d4a69dd910533ad9
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-12-12 22:09:10 +0300

    semi-done is_correct proc

--- lab3/lab3.asm
+++ lab3/lab3.asm
@@ -36,8 +36,10 @@ input macro text ;ввод строки символов
 	inc bx
 	mov bl, [bx] ; первый введенный символ
 	cmp bl, 18h  ; проверка на ввод ctrl+x (= выход)
-	je exit
-
+	jne .not_exit
+	print exit_mess
+	jmp exit
+.not_exit:
 	; возврат состояния из стека
     pop bx
 	pop dx
@@ -68,19 +70,27 @@ in_to_out macro
 endm
 
 .data
-    NUMS_SIZE dw 5
+    NUMS_SIZE        equ 5h
+    NEGATIVE         equ 2Dh
+    MAX_DIGITS_COUNT equ 5h
+    POS_LAST         equ 37h
+    NEG_LAST         equ 38h
+
+    MAX_NUM db '3276' ; старшие разряды модуля максимального числа без последнего разряда
+
     start_mess db 'Input:5 numbers in [-32768, 32767]',10,13, 'Press <Enter> after each number',10,13,'$'
     input_mess db 'Enter number: $'
     empty_mess db 'Input Error: empty string. Try again or use ctrl+x and press enter for exit.', 10, '$'
-    exit_mess db 'Program was aborted by keybord', 10 ,'$'
-    carret db 10, '$'
+    exit_mess  db 'Program was aborted by keybord', 10 ,'$'
+    err_mess db 'Input error!','$'
+    carret     db 10, '$'
+
     tmp_num db 7 dup(0)
-    in_str db 6, ?, 6 dup (?)    ;строка символов (не более 6)
+    in_str db 7, ?, 7 dup (?)    ;строка символов (не более 6)
     out_str db 6 dup (' '),'$'
 
-
-    err_mess db 'Input error!','$'
-    flag_err equ 1
+    neg_flag db 0
+    flag_err db 0
 
 .stack 256
 
@@ -112,7 +122,6 @@ start:
     jmp exit
 
 exit:
-    print exit_mess
     mov ah, 4ch
     mov al, 0
     int 21h
@@ -153,7 +162,7 @@ to_decimal proc
 
     ; проверка на наличие символа "минус", если есть то записываем в выходной буфер без изменений
     ; и двигаем указатель, если нет прыгаем к основному циклу обработки
-    cmp byte ptr [si], 2dh
+    cmp byte ptr [si], NEGATIVE
         jne .cycle
         mov bx, [si]
         xor bh, bh
@@ -181,5 +190,63 @@ to_decimal proc
     ret
 
 to_decimal endp
+
+is_correct proc
+    ; Процедура для проверки корректности введенного числа
+    ; число должно быть записано в буфер in_str
+    ; корректный диапазон - [-32768, 32767]
+
+    push ax ; используется для хранения текущего символа
+    push bx ; используется для хранения индекса в диапазоне
+    push cx ; используется для хранения индекса в числе
+    push dx ; используется для хранения последнего символа диапазона в зависимости от знака
+
+    xor ax, ax
+    xor bx, bx
+    xor cx, cx
+    xor dx, dx
+
+    mov neg_flag, 0 ; флаг для хранения знака числа
+    mov cl, 2      ; Инициализация текущего индекса
+    mov bl, 0
+
+    mov al, in_str[cl]; Загрузка текущего элемента буфера в AL
+    cmp al, NEGATIVE
+    jne .iterate
+    mov neg_flag, 1
+    inc cl
+.is_num_cycle:
+
+; здесь должна быть проверка количества символов если оно максимальное то идем в .range_cycle
+
+.range_cycle:
+    mov al, in_str[cl] ; Загрузка текущего элемента буфера в AL
+    cmp al, MAX_NUM[bx]
+    jne .error
+    inc cl            ; Увеличение индекса числа
+    inc bl            ; Увеличение индекса диапазона
+    cmp cl, in_str[1] ; Проверка, достигнут ли конец буфера
+    je .end_range     ; Если да, прерывание цикла
+    jmp .range_cycle  ; Переход на следующую итерацию цикла
+
+.end_range:           ; проверка последнего символа если число значащих цифр равно максимальному
+    cmp neg_flag, 1
+    je .negative
+    mov dx, POS_LAST
+    jmp .continue
+.negative:
+    mov dx, NEG_LAST
+.continue:
+    cmp dx, al
+    jne .error        ; если есть выход за границу диапазона переход к ошибке
+    jmp .corr_end
+
+.error:
+    print overflow_mess
+    mov err_flag, 1
+
+.corr_end:
+    ret
+is_correct endp
 
 end start
